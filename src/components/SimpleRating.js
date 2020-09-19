51d6a3f5467c655f5ab198f9821568ead6b723d5
Jnmendza <jonatmendoza@gmail.com>
2020-09-19 03:25:59 -0700

    modal is up with Title, overview and new rating system-read only

---
+++ src/components/SimpleRating.js
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+// import Typography from '@material-ui/core/Typography';
+import Box from '@material-ui/core/Box';
+import Rating from '@material-ui/lab/Rating';
+
+const SimpleRating = ({ rating }) => {
+    const starRating = (rating / 10) * 5
+    const [value, setValue] = useState(starRating)
+    
+    return(
+        <div>
+            <Box component="fieldset" mb={3} borderColor="transparent">
+                {/* <Typography component="legend">Read only</Typography> */}
+                <Rating name="half-rating-read" defaultValue={value} precision={0.5} readOnly />
+                <p>{rating} / 10</p>
+            </Box>
+    </div>
+    )
+}
+
+export default SimpleRating
--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -1,13 +1,48 @@
-import React, { forwardRef } from 'react'
+import React, { forwardRef, useState } from 'react'
 import "./VideoCard.css"
 import TextTruncate from 'react-text-truncate'
 import { ThumbUpSharp } from '@material-ui/icons'
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+
+import SimpleRating from './SimpleRating'
+
+
+const useStyles = makeStyles((theme) => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(2, 4, 3),
+    },
+    
+  }));
 
 const VideoCard = forwardRef(({ movie }, ref) => {
+    console.log(movie)
+    // Modal Logic
+    const classes = useStyles();
+    const [open, setOpen] = useState(false)
+
+    const handleOpen = () => {
+        setOpen(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+
     const base_url = "https://image.tmdb.org/t/p/original/"
 
     return (
-        <div ref={ref} className="videoCard">
+        <div ref={ref} className="videoCard" onClick={handleOpen}>
             <img src={`${base_url}${movie.backdrop_path || movie.poster_path}`} alt="movie poster" />
             <TextTruncate 
                 line={2}
@@ -17,10 +52,33 @@ const VideoCard = forwardRef(({ movie }, ref) => {
             />
             <h2>{movie.title || movie.original_name}</h2>
             <p className='videoCard__stats'>
-            {movie.media_type && `${movie.media_type} •`}
-            {movie.release_date || movie.first_air_date} •
-            <ThumbUpSharp /> {"  "}
-            {movie.vote_count}</p>
+                {movie.media_type && `${movie.media_type} •`}
+                {movie.release_date || movie.first_air_date} •
+                <ThumbUpSharp /> {"  "}
+                {movie.vote_count}
+            </p>
+
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                className={classes.modal}
+                onClose={handleClose}
+                open={open}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                <div className={classes.paper}>
+                    <h2 id="transition-modal-title">{movie.title}</h2>
+                    <p id="transition-modal-description">{movie.overview}</p>
+                    <SimpleRating rating={movie.vote_average}/>
+                </div>
+                </Fade>
+            </Modal>
+
         </div>
     )
 });
