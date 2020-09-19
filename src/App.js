4bc76a25ea60ae57402a801480e4f9af7a6cf9d5
Jnmendza <jonatmendoza@gmail.com>
2020-09-18 23:18:12 -0700

    nav and beginning of axios requests

--- src/App.js
+++ src/App.js
@@ -2,11 +2,15 @@ import React from 'react';
 import './App.css';
 
 import Header from './components/Header'
+import Nav from './components/Nav'
+import Results from './components/Results'
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Nav />
+      <Results />
     </div>
   );
 }
